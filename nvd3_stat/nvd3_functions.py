4a6eadeb475c2e9179fffc604a4ce7ae9325f909
Bernhard Walter <b_walter@arcor.de>
2017-03-27 08:59:12 +0200

    implemented proper ipykernel Communication

--- nvd3_stat/nvd3_functions.py
+++ nvd3_stat/nvd3_functions.py
@@ -27,217 +27,216 @@ else:
 class Nvd3Functions(object):
     
     makeChart = """
-        
-makeChart = function(session, object, chart) {
+    function(session, object, chart) {
 
-    var cacheId = "__nv__chart_cache_" + object.plotId;
+        var cacheId = "__nv__chart_cache_" + object.plotId;
 
-    var Logger = function(name) {
-        this.name = name;
-    }
-    Logger.prototype.info = function(msg) {
-        if (window.__nvd3_stat_debug > 0) {
-            console.info(this.name + " [INFO] " + msg)
+        var Logger = function(name) {
+            this.name = name;
         }
-    }
-    Logger.prototype.debug = function(msg, obj) {
-        if (window.__nvd3_stat_debug > 1) {
-            if (typeof(obj) === "undefined") {
-                console.log(this.name + " [DEBUG] " + msg)
-            } else {
-                console.log(this.name + " [DEBUG] " + msg, obj)
+        Logger.prototype.info = function(msg) {
+            if (window.__nvd3_stat_debug > 0) {
+                console.info(this.name + " [INFO] " + msg)
             }
         }
-    }
-    var logger = new Logger("NVD3-Stat");
-    
-    logger.debug("makeChart called");
-
-    d3.selectAll('.nvtooltip').style('opacity', '0');  // remove all "zombie" tooltips
-
-    nv.utils.windowResize = function(chart) { console.info("windowResize not supported") } // avoid d3 translate(Nan,5) errors
-    var duration = 350;
-
-
-    var identifyData = function(data) {
-        if ((typeof(data[0].values) != "undefined") && (typeof(data[0].key) != "undefined")) {
-            return "kv"
-        } else if ((typeof(data[0].x) != "undefined") && (typeof(data[0].y) != "undefined")) {
-            return "xy"
-        } else {
-            console.error('Unknown data type')
+        Logger.prototype.debug = function(msg, obj) {
+            if (window.__nvd3_stat_debug > 1) {
+                if (typeof(obj) === "undefined") {
+                    console.log(this.name + " [DEBUG] " + msg)
+                } else {
+                    console.log(this.name + " [DEBUG] " + msg, obj)
+                }
+            }
         }
-    }
+        var logger = new Logger("NVD3-Stat");
+        
+        logger.debug("makeChart called");
+
+        d3.selectAll('.nvtooltip').style('opacity', '0');  // remove all "zombie" tooltips
+
+        nv.utils.windowResize = function(chart) { console.info("windowResize not supported") } // avoid d3 translate(Nan,5) errors
+        var duration = 350;
 
 
-    var configure = function(chartModel, config) {
-        for (var c in config) {
-            if (c == "margin" || c == "arcRadius") {
-                chart[c](config[c]);                
-            } else if ((typeof(config[c]) === "object") && ! Array.isArray(config[c])) {       // sub config, 1 level
-                for (var c2 in config[c]) {
-                    if (c2 == "tickFormat") {
-                        var format = config[c][c2];
-                        if (format[0] == "%") {
-                            chart[c][c2](function(d) { return d3.time.format(format)(new Date(d)) });
+        var identifyData = function(data) {
+            if ((typeof(data[0].values) != "undefined") && (typeof(data[0].key) != "undefined")) {
+                return "kv"
+            } else if ((typeof(data[0].x) != "undefined") && (typeof(data[0].y) != "undefined")) {
+                return "xy"
+            } else {
+                console.error('Unknown data type')
+            }
+        }
+
+
+        var configure = function(chartModel, config) {
+            for (var c in config) {
+                if (c == "margin" || c == "arcRadius") {
+                    chart[c](config[c]);                
+                } else if ((typeof(config[c]) === "object") && ! Array.isArray(config[c])) {       // sub config, 1 level
+                    for (var c2 in config[c]) {
+                        if (c2 == "tickFormat") {
+                            var format = config[c][c2];
+                            if (format[0] == "%") {
+                                chart[c][c2](function(d) { return d3.time.format(format)(new Date(d)) });
+                            } else {
+                                chart[c][c2](d3.format(format));
+                            }
                         } else {
-                            chart[c][c2](d3.format(format));
+                            chart[c][c2](config[c][c2]);
                         }
+                    }
+                } else {                                                                     // direct config
+                    if (c === "halfPie" && config[c]) {
+                        chart.pie.startAngle(function(d) { return d.startAngle/2 - Math.PI/2 })
+                                 .endAngle(function(d)   { return d.endAngle/2   - Math.PI/2 });
                     } else {
-                        chart[c][c2](config[c][c2]);
+                        chart[c](config[c]);
                     }
                 }
-            } else {                                                                     // direct config
-                if (c === "halfPie" && config[c]) {
-                    chart.pie.startAngle(function(d) { return d.startAngle/2 - Math.PI/2 })
-                             .endAngle(function(d)   { return d.endAngle/2   - Math.PI/2 });
-                } else {
-                    chart[c](config[c]);
-                }
             }
-        }
-        return chart;
-    }
-
-    
-    if (object.event == "plot") {
-        logger.debug("plot " + object.plotId, object.data)
-
-        var data = object.data.data;
-        var config = object.data.config;
-
-        var divId = "#" + object.plotId + " svg";
-        session[cacheId] = {"data": data};
-
-        nv.addGraph(function() {
-            try {
-                chart = configure(chart, config);
-                chartData = d3.select(divId).datum(JSON.parse(JSON.stringify(data)));
-                chartData.transition().duration(500).call(chart);
-            } catch(err) {
-                console.error(err.message);
-            }
-
-            
-            session[cacheId].chart = chart;
-            session[cacheId].chartData = chartData;
-
             return chart;
-        })
+        }
 
-                           
-    } else if (object.event == "saveAsPng") {
-        logger.debug("Save " + object.plotId + " as PNG")
-        window.saveSvgAsPng(document.getElementById(object.plotId).children[0], object.data.filename + ".png", {backgroundColor: object.data.backgroundColor});
+        
+        if (object.event == "plot") {
+            logger.debug("plot " + object.plotId, object.data)
 
-    } else if (object.event == "replace") {
-        var data = JSON.parse(JSON.stringify(object.data));  // clone data
+            var data = object.data.data;
+            var config = object.data.config;
+
+            var divId = "#" + object.plotId + " svg";
+            session[cacheId] = {"data": data};
+
+            nv.addGraph(function() {
+                try {
+                    chart = configure(chart, config);
+                    chartData = d3.select(divId).datum(JSON.parse(JSON.stringify(data)));
+                    chartData.transition().duration(500).call(chart);
+                } catch(err) {
+                    console.error(err.message);
+                }
+
+                
+                session[cacheId].chart = chart;
+                session[cacheId].chartData = chartData;
+
+                return chart;
+            })
+
+                               
+        } else if (object.event == "saveAsPng") {
+            logger.debug("Save " + object.plotId + " as PNG")
+            window.saveSvgAsPng(document.getElementById(object.plotId).children[0], object.data.filename + ".png", {backgroundColor: object.data.backgroundColor});
+
+        } else if (object.event == "replace") {
+            var data = JSON.parse(JSON.stringify(object.data));  // clone data
 
 
-        session[cacheId].data = data;
-        var chart = session[cacheId].chart;
-        var chartData = session[cacheId].chartData;
-        console.log("replace: " + chart.container.parentNode.id)
+            session[cacheId].data = data;
+            var chart = session[cacheId].chart;
+            var chartData = session[cacheId].chartData;
+            console.log("replace: " + chart.container.parentNode.id)
 
-        chartData.datum(data).transition().duration(duration).call(chart);
+            chartData.datum(data).transition().duration(duration).call(chart);
 
-    } else if (object.event == "append") {
+        } else if (object.event == "append") {
 
-        var newData = JSON.parse(JSON.stringify(object.data));  // clone data
+            var newData = JSON.parse(JSON.stringify(object.data));  // clone data
 
-        var data = session[cacheId].data;
+            var data = session[cacheId].data;
 
-        var _append = function(dataType, dold, dnew) {
+            var _append = function(dataType, dold, dnew) {
+                if (dataType == "kv") {
+                    dold.values = dold.values.concat(dnew.values);
+                    for (var attr in dnew) {
+                        if (attr !== "key" && attr !== "values") {
+                            dold[attr] = dnew[attr];
+                        }
+                    }
+                } else {
+                    dold = dold.concat(dnew);
+                }
+                return dold
+            }
+            
+            var dataType = identifyData(data);
             if (dataType == "kv") {
-                dold.values = dold.values.concat(dnew.values);
-                for (var attr in dnew) {
+                for (var i in data) {
+                    data[i] = _append("kv", data[i], newData[i])
+                }
+            } else if (dataType == "xy") {
+                data = _append(dataType, data, newData)
+            } else {
+                console.error("Unknown data type");
+            }
+
+            var chart = session[cacheId].chart;
+            var chartData = session[cacheId].chartData;
+            logger.debug("append: " + chart.container.parentNode.id, newData)
+            
+            chartData.datum(data).transition().duration(duration).call(chart);
+
+        } else if (object.event == "update") {
+
+            var changedData = JSON.parse(JSON.stringify(object.data.changedData));  // clone data
+            var rowIndices = object.data.rowIndices;
+            
+            var data = session[cacheId].data;
+            
+            var _update = function(dold, dnew, rowIndices) {
+                for (var i in rowIndices) {
+                    dold.values[rowIndices[i]] = dnew.values[i];
+                }
+                for (attr in dnew) {
                     if (attr !== "key" && attr !== "values") {
                         dold[attr] = dnew[attr];
                     }
                 }
+            }
+
+            if (data instanceof Array && changedData instanceof Array) {
+                for (i in data) { _update(data[i], changedData[i], rowIndices) }
             } else {
-                dold = dold.concat(dnew);
+                _update(data, changedData, rowIndices)
             }
-            return dold
-        }
-        
-        var dataType = identifyData(data);
-        if (dataType == "kv") {
-            for (var i in data) {
-                data[i] = _append("kv", data[i], newData[i])
-            }
-        } else if (dataType == "xy") {
-            data = _append(dataType, data, newData)
-        } else {
-            console.error("Unknown data type");
-        }
+            
+            var chart = session[cacheId].chart;
+            var chartData = session[cacheId].chartData;
+            logger.debug("update: " + chart.container.parentNode.id, changedData)
 
-        var chart = session[cacheId].chart;
-        var chartData = session[cacheId].chartData;
-        logger.debug("append: " + chart.container.parentNode.id, newData)
-        
-        chartData.datum(data).transition().duration(duration).call(chart);
+            chartData.datum(data).transition().duration(duration).call(chart);
+            
+        } else if (object.event == "delete") {
 
-    } else if (object.event == "update") {
+            var sortedIndices = object.data.rowIndices;
 
-        var changedData = JSON.parse(JSON.stringify(object.data.changedData));  // clone data
-        var rowIndices = object.data.rowIndices;
-        
-        var data = session[cacheId].data;
-        
-        var _update = function(dold, dnew, rowIndices) {
-            for (var i in rowIndices) {
-                dold.values[rowIndices[i]] = dnew.values[i];
-            }
-            for (attr in dnew) {
-                if (attr !== "key" && attr !== "values") {
-                    dold[attr] = dnew[attr];
+            var data = session[cacheId].data;
+
+            var _delete = function(dold, rowIndices) {
+                for (var i in sortedIndices) {
+                    dold.values.splice(sortedIndices[i], 1);
                 }
             }
-        }
 
-        if (data instanceof Array && changedData instanceof Array) {
-            for (i in data) { _update(data[i], changedData[i], rowIndices) }
-        } else {
-            _update(data, changedData, rowIndices)
-        }
-        
-        var chart = session[cacheId].chart;
-        var chartData = session[cacheId].chartData;
-        logger.debug("update: " + chart.container.parentNode.id, changedData)
-
-        chartData.datum(data).transition().duration(duration).call(chart);
-        
-    } else if (object.event == "delete") {
-
-        var sortedIndices = object.data.rowIndices;
-
-        var data = session[cacheId].data;
-
-        var _delete = function(dold, rowIndices) {
-            for (var i in sortedIndices) {
-                dold.values.splice(sortedIndices[i], 1);
+            if (data instanceof Array) {
+                for (var i in data) { _delete(data[i], sortedIndices) }
+            } else {
+                _delete(data, sortedIndices)
             }
-        }
+            
+            var chart = session[cacheId].chart;
+            var chartData = session[cacheId].chartData;
+            logger.debug("delete: " + chart.container.parentNode.id, sortedIndices)
 
-        if (data instanceof Array) {
-            for (var i in data) { _delete(data[i], sortedIndices) }
-        } else {
-            _delete(data, sortedIndices)
-        }
-        
-        var chart = session[cacheId].chart;
-        var chartData = session[cacheId].chartData;
-        logger.debug("delete: " + chart.container.parentNode.id, sortedIndices)
-
-        chartData.datum(data).transition().duration(duration).call(chart);
-        chart.update()
-    }}
-"""
+            chartData.datum(data).transition().duration(duration).call(chart);
+            chart.update()
+        }}
+    """
 
     def __init__(self):
         self.session = Session()
-        self.session.registerFunction("makeChart", Nvd3Functions.makeChart.lstrip())
+        self.register("makeChart", Nvd3Functions.makeChart)
 
         js = """
         if(typeof(window.__nvd3_stat_debug) == "undefined") {
@@ -249,7 +248,7 @@ makeChart = function(session, object, chart) {
 
 
     def register(self, funcName, funcBody):
-        self.session.registerFunction(funcName, "%s = %s" % (funcName.lstrip(), funcBody)) 
+        self.session.registerFunction(funcName.lstrip(), funcBody.lstrip()) 
 
  
     def call(self, funcName, event, data, divId, delay=200):
--- nvd3_stat/utils/ipython.py
+++ nvd3_stat/utils/ipython.py
@@ -13,42 +13,68 @@
 # limitations under the License.
 
 from IPython.display import Javascript, display_javascript, HTML, display_html
+from ipykernel.comm import Comm
 import json
+import time
 
 
 class IPythonSession(object):
 
     def __init__(self):
+        self.comm = None
+
         js = """
-            window.Nvd3py = function() {};
-            window.nvd3py = new window.Nvd3py();
-            window.nvd3py.session = {};
-            window.nvd3py.session.__functions = {};
-            """
+            window.nvd3_stat = { session:{ __functions:{} } };
+        """
         display_javascript(Javascript(js))
 
         css = """
-        <style>
-        div.output_area img, div.output_area svg {
-            max-width: 100%;
-            height: 100%;
-        }
-        </style>
+            <style>
+                div.output_area img, div.output_area svg {
+                    max-width: 100%;
+                    height: 100%;
+                }
+            </style>
         """
         display_html(HTML(css))
             
 
-    def registerFunction(self, funcName, func):
+    def registerFunction(self, funcName, funcBody):
         js = """
-        window.Nvd3py.prototype.%s;
-        window.nvd3py.session.__functions.%s = window.nvd3py.%s;
-        """ % (func, funcName, funcName)
+        window.nvd3_stat.session.__functions.%s = %s;
+        """ % (funcName, funcBody)
         display_javascript(Javascript(js))
 
+        if funcName == "makeChart":
+            js = """
+            Jupyter.notebook.kernel.comm_manager.register_target("nvd3_stat", 
+                function(comm, msg) {
+                    console.log("NVD3-Stat [INFO] Comm for nvd3_stat opened ", msg);
 
-    def call(aself, funcName, args, delay):
-        payload = json.dumps(args)
-        display_javascript(Javascript("window.nvd3py." + funcName + "(window.nvd3py.session, " + payload + ")"))
+                    comm.on_msg(function(msg) {
+                        var funcName = msg.content.data.funcName;
+                        var args = msg.content.data.args;
+                        if (window.__nvd3_stat_debug > 1) {
+                            console.log("NVD3-Stat [DEBUG]", funcName, args)
+                        }
+                        window.nvd3_stat.session.__functions[funcName](window.nvd3_stat.session, args);
+                    });
+
+                    comm.on_close(function(msg) {
+                        console.log("NVD3-Stat [INFO] comm for nvd3_stat closed ", msg);         
+                    });    
+                }    
+            )
+            """
+
+            display_javascript(Javascript(js))
+
+            time.sleep(0.5)
+            self.comm = Comm(target_name='nvd3_stat', data={'event': 'open'})
+
+
+    def call(self, funcName, args, delay):
+        self.comm.send({"funcName":funcName, "args":args, "delay": delay})
 
 
 
