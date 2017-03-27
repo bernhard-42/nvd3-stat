9b61a2c774ee17c3a9387c3a3895b11bff21ee03
Bernhard Walter <b_walter@arcor.de>
2017-03-27 09:06:51 +0200

    improved javascript initialisation

--- nvd3_stat/utils/ipython.py
+++ nvd3_stat/utils/ipython.py
@@ -25,31 +25,10 @@ class IPythonSession(object):
 
         js = """
             window.nvd3_stat = { session:{ __functions:{} } };
-        """
-        display_javascript(Javascript(js))
 
-        css = """
-            <style>
-                div.output_area img, div.output_area svg {
-                    max-width: 100%;
-                    height: 100%;
-                }
-            </style>
-        """
-        display_html(HTML(css))
-            
-
-    def registerFunction(self, funcName, funcBody):
-        js = """
-        window.nvd3_stat.session.__functions.%s = %s;
-        """ % (funcName, funcBody)
-        display_javascript(Javascript(js))
-
-        if funcName == "makeChart":
-            js = """
             Jupyter.notebook.kernel.comm_manager.register_target("nvd3_stat", 
                 function(comm, msg) {
-                    console.log("NVD3-Stat [INFO] Comm for nvd3_stat opened ", msg);
+                    console.log("NVD3-Stat [INFO] Comm for nvd3_stat opened");
 
                     comm.on_msg(function(msg) {
                         var funcName = msg.content.data.funcName;
@@ -65,12 +44,28 @@ class IPythonSession(object):
                     });    
                 }    
             )
-            """
+        """
+        display_javascript(Javascript(js))
 
-            display_javascript(Javascript(js))
+        css = """
+            <style>
+                div.output_area img, div.output_area svg {
+                    max-width: 100%;
+                    height: 100%;
+                }
+            </style>
+        """
+        display_html(HTML(css))
+            
+        time.sleep(0.2)
+        self.comm = Comm(target_name='nvd3_stat', data={'event': 'open'})
 
-            time.sleep(0.5)
-            self.comm = Comm(target_name='nvd3_stat', data={'event': 'open'})
+
+    def registerFunction(self, funcName, funcBody):
+        js = """
+        window.nvd3_stat.session.__functions.%s = %s;
+        """ % (funcName, funcBody)
+        display_javascript(Javascript(js))
 
 
     def call(self, funcName, args, delay):
@@ -81,63 +76,63 @@ class IPythonSession(object):
 def loadNVD3(nvd3version="1.8.2", d3version="3.5.17"):
 
     js = """
-    var d3_js = "https://cdnjs.cloudflare.com/ajax/libs/d3/%s/d3";
-    var nvd3_css = "https://cdnjs.cloudflare.com/ajax/libs/nvd3/%s/nv.d3.css";
-    var nvd3_js = "https://cdnjs.cloudflare.com/ajax/libs/nvd3/%s/nv.d3.js";
-    var saveAsPng_js = "https://rawgit.com/bernhard-42/saveSvgAsPng/gh-pages/saveSvgAsPng";
+        var d3_js = "https://cdnjs.cloudflare.com/ajax/libs/d3/%s/d3";
+        var nvd3_css = "https://cdnjs.cloudflare.com/ajax/libs/nvd3/%s/nv.d3.css";
+        var nvd3_js = "https://cdnjs.cloudflare.com/ajax/libs/nvd3/%s/nv.d3.js";
+        var saveAsPng_js = "https://rawgit.com/bernhard-42/saveSvgAsPng/gh-pages/saveSvgAsPng";
 
-    var cssLoaded = function(href) {
-        var found = false;
-        for (var i in document.styleSheets) {
-            if (document.styleSheets[i].href == href ) {
-                found = true;
-                break;
-            }
-        }   
-        return found;
-    }
+        var cssLoaded = function(href) {
+            var found = false;
+            for (var i in document.styleSheets) {
+                if (document.styleSheets[i].href == href ) {
+                    found = true;
+                    break;
+                }
+            }   
+            return found;
+        }
 
-    var loadCss = function(href, callback) {
-       $('<link/>', {
-            rel: 'stylesheet',
-            type: 'text/css',
-            crossOrigin: 'anonymous',
-            href: href
-        }).error(function() {
-            element.html("<div style='color:red'>Error: loading nv.d3.css</div>");
-        }).load(function() {
-            if (cssLoaded(href)) {
-                element.append("<div>loaded nvd3 css</div>");
-                loadJs();
-            }
-        }).appendTo('head');
-    }
+        var loadCss = function(href, callback) {
+           $('<link/>', {
+                rel: 'stylesheet',
+                type: 'text/css',
+                crossOrigin: 'anonymous',
+                href: href
+            }).error(function() {
+                element.html("<div style='color:red'>Error: loading nv.d3.css</div>");
+            }).load(function() {
+                if (cssLoaded(href)) {
+                    element.append("<div>loaded nvd3 css</div>");
+                    loadJs();
+                }
+            }).appendTo('head');
+        }
 
-    var loadJs = function() {
-        require.config({ paths: {d3: d3_js, saveSvg: saveAsPng_js} });
-        require(["d3"], function(d3) {
-            window.d3 = d3;
-            element.append("<div>loaded d3 js " + d3.version + "</div>", window.d3)
-            $.getScript(nvd3_js)
-             .done(function( script, textStatus ) {
-                element.append("<div>loaded nvd3.js " + nv.version + "</div>", window.nv)
-                require(["saveSvg"], function(saveSvgAsPng) {
-                    window.saveSvgAsPng = saveSvgAsPng.saveSvgAsPng
-                    window.saveSvg = saveSvgAsPng.saveSvg
-                    window.__saveSvgAsPng = saveSvgAsPng;
-                    element.append("<div>loaded saveSvgAsPng</div>");
-                })
-             }).fail(function(jqxhr, settings, exception){
-                element.html("<div style='color:red'>Error: loading nv.d3.js</div>");
-             })
-        });
-    }
+        var loadJs = function() {
+            require.config({ paths: {d3: d3_js, saveSvg: saveAsPng_js} });
+            require(["d3"], function(d3) {
+                window.d3 = d3;
+                element.append("<div>loaded d3 js " + d3.version + "</div>", window.d3)
+                $.getScript(nvd3_js)
+                 .done(function( script, textStatus ) {
+                    element.append("<div>loaded nvd3.js " + nv.version + "</div>", window.nv)
+                    require(["saveSvg"], function(saveSvgAsPng) {
+                        window.saveSvgAsPng = saveSvgAsPng.saveSvgAsPng
+                        window.saveSvg = saveSvgAsPng.saveSvg
+                        window.__saveSvgAsPng = saveSvgAsPng;
+                        element.append("<div>loaded saveSvgAsPng</div>");
+                    })
+                 }).fail(function(jqxhr, settings, exception){
+                    element.html("<div style='color:red'>Error: loading nv.d3.js</div>");
+                 })
+            });
+        }
 
-    if (cssLoaded(nvd3_css)) {
-        loadJs();
-    } else {
-        loadCss(nvd3_css, loadJs);
-    } 
+        if (cssLoaded(nvd3_css)) {
+            loadJs();
+        } else {
+            loadCss(nvd3_css, loadJs);
+        } 
     """ % (d3version, nvd3version, nvd3version)
     
     display_javascript(Javascript(js))
