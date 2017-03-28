0dd927f391bd45a2d068e859b6566ac4201c3ac4
bernhard <bernhard.walter@me.com>
2017-03-28 13:36:14 +0200

    slicing for colors

--- nvd3_stat/__init__.py
+++ nvd3_stat/__init__.py
@@ -117,25 +117,36 @@ class Nvd3(object):
         return BulletChart(self.nvd3Functions)
 
 
+    def _slice(colors, args):
+        if len(args) == 0:
+            return colors
+        else:
+            return [colors[i%len(colors)] for i in args]
+        
     # Source: http://d3js.org
-    def c10(self):
-        return ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
+    def c10(self, *args):
+        colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
+        return self._slice(colors, args)
 
     # Source: http://d3js.org
-    def c20(self):
-        return ["#1f77b4", "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c", "#98df8a", "#d62728", "#ff9896", "#9467bd", "#c5b0d5", 
+    def c20(self, *args):
+        colors = ["#1f77b4", "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c", "#98df8a", "#d62728", "#ff9896", "#9467bd", "#c5b0d5", 
                 "#8c564b", "#c49c94", "#e377c2", "#f7b6d2", "#7f7f7f", "#c7c7c7", "#bcbd22", "#dbdb8d", "#17becf", "#9edae5"]
+        return self._slice(colors, args)
 
     # Source: http://d3js.org
-    def c20b(self):
-        return ["#393b79", "#5254a3", "#6b6ecf", "#9c9ede", "#637939", "#8ca252", "#b5cf6b", "#cedb9c", "#8c6d31", "#bd9e39", 
+    def c20b(self, *args):
+        colors = ["#393b79", "#5254a3", "#6b6ecf", "#9c9ede", "#637939", "#8ca252", "#b5cf6b", "#cedb9c", "#8c6d31", "#bd9e39", 
                 "#e7ba52", "#e7cb94", "#843c39", "#ad494a", "#d6616b", "#e7969c", "#7b4173", "#a55194", "#ce6dbd", "#de9ed6"]
+        return self._slice(colors, args)
 
     # Source: http://d3js.org
-    def c20c(self):
-        return ["#3182bd", "#6baed6", "#9ecae1", "#c6dbef", "#e6550d", "#fd8d3c", "#fdae6b", "#fdd0a2", "#31a354", "#74c476", 
+    def c20c(self, *args):
+        colors = ["#3182bd", "#6baed6", "#9ecae1", "#c6dbef", "#e6550d", "#fd8d3c", "#fdae6b", "#fdd0a2", "#31a354", "#74c476", 
                 "#a1d99b", "#c7e9c0", "#756bb1", "#9e9ac8", "#bcbddc", "#dadaeb", "#636363", "#969696", "#bdbdbd", "#d9d9d9"]
+        return self._slice(colors, args)
 
     def grey(self):
         digits = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"]
-        return ["#%s" % (d*3) for d in digits]
+        colors = ["#%s" % (d*3) for d in digits]
+        return self._slice(colors, args)
